817f6e2f37486429d6cbb8e37d12de4798cbd798
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2024-12-05 19:34:22 +0530

    update code

--- Lab_7/q2.c
+++ Lab_7/q2.c
@@ -6,35 +6,38 @@ int main(void)
 {
 	long int x;
 	int i, j;
-	
+
 	SystemInit();
 	SystemCoreClockUpdate();
-	
-	LPC_PINCON -> PINSEL0 = 0;		// switch
-	LPC_PINCON -> PINSEL1 = 0;		// led	
-	LPC_GPIO0 -> FIODIR = 0xFF << 15 | 0 << 10;	// P0.15 to P0.22 output direction
-	
+
+	LPC_PINCON->PINSEL0 = 0; // GPIO mode P0.4 to P0.11
+	LPC_PINCON->PINSEL4 = 0; // P2.12
+
+	LPC_GPIO0->FIODIR = 0xFF << 4; // P0.4 to P0.11 output direction
+	LPC_GPIO2->FIODIR = 0;		   // input P2.12
+
 	i = 0;
-	
-	while(1)
+
+	while (1)
 	{
-		x = LPC_GPIO0 -> FIOPIN;
-		x = x & 1 << 10;
-		if(x == 0) // switch pressed Down counter
+		x = LPC_GPIO2->FIOPIN;
+		x = x & 1 << 12;
+		if (x == 0) // switch pressed Down counter
 		{
 			i = i - 1;
-			if(i == -1)
+			if (i == -1)
 				i = 255;
 		}
-		
+
 		else // UP counter
 		{
 			i = i + 1;
-			if(i == 256)
+			if (i == 256)
 				i = 0;
 		}
-		
-		LPC_GPIO0 -> FIOPIN = i << 15;
-		for(j = 0; j < 1000000; j++);  // delay
+
+		LPC_GPIO0->FIOPIN = i << 4;
+		for (j = 0; j < 1000000; j++)
+			; // delay
 	}
 }
